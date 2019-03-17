0595b9fcd1b39734932ee64ed7e09608672e5107
Thomas Lisankie <thomas@tomlisankie.com>
2019-03-17 15:43:07 -0400

    Updated versions of dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject cljs-bach "0.4.0-SNAPSHOT"
   :description "A Clojurescript wrapper for the Web Audio API."
-  :license {:name "MIT" }
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.228"]
-                 [cljs-ajax "0.5.5"]]
+  :license {:name "MIT"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [cljs-ajax "0.8.0"]]
 
-  :min-lein-version "2.5.0"
+  :min-lein-version "2.9.0"
 
-  :plugins [[lein-cljsbuild "1.1.2"]
-            [lein-figwheel "0.5.0-2"]]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-figwheel "0.5.18"]]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled"
                                     "target"
